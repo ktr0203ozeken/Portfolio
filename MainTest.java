8b22a89100702c5a8d93b90a8234ed4d94acf7a5
ktr0203ozeken <ktr.0203.ozeken@gmail.com>
2025-03-31 19:21:45 +0900

    WriteAppの動作を確認した

---
+++ MainTest.java
@@ -0,0 +1,23 @@
+public class MainTest {
+    public static void main(String[] args) {
+
+        //WriteAppの動作確認
+        WriteApp writeApp =new WriteApp();
+
+        //writeApp.sendMessage
+        int messageId=writeApp.sendMessage("こんにちは");
+        System.out.println(messageId);
+
+        //writeApp.getMessage
+        String message=writeApp.getMessage(1);
+        System.out.println(message);
+       
+        //writeApp.deleteMessage
+        writeApp.deleteMessage(1);
+
+        //writeApp.getMessage
+        //削除後のメッセージ取得の動作確認
+        String message2=writeApp.getMessage(1);
+        System.out.println(message2);
+    }
+}
