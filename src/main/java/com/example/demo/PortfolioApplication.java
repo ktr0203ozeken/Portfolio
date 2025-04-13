568cbd41d481640e28ae51d3587c6e8faaa681b8
ktr0203ozeken <ktr.0203.ozeken@gmail.com>
2025-04-14 00:40:17 +0900

    -MessageController.javaとmessage-form.htmlの作成
    -gradleファイルにthymeleafの依存関係を追加
    
    -未使用のためPortfolioApplication.javaのコードを削除

--- src/main/java/com/example/demo/PortfolioApplication.java
+++ src/main/java/com/example/demo/PortfolioApplication.java
@@ -1,39 +1,12 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PortfolioApplication implements CommandLineRunner {
+public class PortfolioApplication{
 
     public static void main(String[] args) {
         SpringApplication.run(PortfolioApplication.class, args);
-    }
-
-    // 依存性をDIで注入
-    @Autowired
-    private MessageService messageService;
-
-    @Override
-    public void run(String... args) throws Exception {
-    	
-        long messageId;
-        
-        //メッセージの送信
-        messageId=messageService.sendMessage("こんにちは");
-        System.out.println(messageId);
-
-        //メッセージの受け取り
-        String message = messageService.getMessage(messageId);
-        System.out.println(message);
-
-        // メッセージ削除
-        messageService.deleteMessage(messageId);
-        
-      //削除後のメッセージ取得の動作確認
-        message=messageService.getMessage(messageId);
-        System.out.println(message);
     }
 }
